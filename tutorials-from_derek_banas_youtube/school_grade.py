fa523acc4d3f6490c95cbb1043e22b779b16db8e
Mthobeki <mthobekisikhosanag@gmail.com>
2023-10-12 10:40:09 +0200

    my files

--- tutorials-from_derek_banas_youtube/school_grade.py
+++ tutorials-from_derek_banas_youtube/school_grade.py
@@ -10,4 +10,11 @@ age = eval(input("How old are you?: "))
 if age <= 5:
     print("Go to kinderdarten")
 
+#handle if age is 6-17
+elif age ==6 or age <=17:
+    grade = age - 5
+    print("Go to grade", format(grade))
 
+# Handle if age is greater than 17
+else:
+    print("Go to college")
