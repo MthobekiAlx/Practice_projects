e086d8b8f8b4127b487bbec56898a5124cbc0645
Mthobeki <mthobekisikhosanag@gmail.com>
2023-10-13 15:24:12 +0200

    my files

---
+++ tutorials-from_derek_banas_youtube/Uppercase_to_code.py
@@ -0,0 +1,8 @@
+#Enter a string to hide in uppercase: HIDE
+samp_string = "HIDE"
+
+#secret message in numbers, that is covert to numbers
+print(ord("H"),ord("I"),ord("D"),ord("E"))
+
+#then print back the original messsage from numbers to letters
+
