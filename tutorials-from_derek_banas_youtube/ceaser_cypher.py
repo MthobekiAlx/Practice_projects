3fd85844689f79ab273cce01803a7bff2a5247af
Mthobeki <mthobekisikhosanag@gmail.com>
2023-10-24 13:48:11 +0200

    update

--- tutorials-from_derek_banas_youtube/ceaser_cypher.py
+++ tutorials-from_derek_banas_youtube/ceaser_cypher.py
@@ -8,32 +8,44 @@
 ceaser_string = input("Enter character to hide: ")
 
 #store said input
-secret_string = ""
+secret_cstrinng = ""
 
 #recieve unit to shift with
-shift_num = eval(input("Please enter the number to shift with: "))
+shift_num = eval(input("Please enter the number to shift between 0 and 26: "))
 
 shift_num = int(shift_num)
 
 print("num is: ", shift_num)
 
+#maybe im thinking about it too hard but we check if we adding or subtracting
+operator = input("Enter '+' or '-' for encryption : ")
+
 
 #use isupper to check which unicode to work with
 if ceaser_string.isupper():
     for char in ceaser_string:
-        secret_string += str(ord(char))
-    print("message before ecyripting is : ", secret_string)
+        secret_cstrinng += str(ord(char))
+    print("message before ecyripting is : ", secret_cstrinng)
+
+    ceaser_string = ""
+    for i in range(0, len(secret_cstrinng)-1, 2):
+        char_code = secret_cstrinng[i] + secret_cstrinng[i+1] #get the 1st and 2nd numbers of each respective pair from code
+        ceaser_string += chr(int(char_code)) 
 
 else: #works for both upper and lower and the combination of 
-    for char in samp_string:
-        secret_string += str(ord(char) - 22) #stores each character code in new string
-    print("Secret message is: ", secret_string) #prints the secret message in code
+    for char in ceaser_string:
+        secret_cstrinng += str(ord(char) - 38) #stores each character code in new string
+    print("Secret message is: ", secret_cstrinng) #prints the secret message in code
     
-    samp_string = ""
+    ceaser_string = ""
     #cycle through each character code 2 numbers at a time since the assigned values come in pairs from 65-90 and we need to seperate them back pairs
-    for i in range(0, len(secret_string)-1, 2):
-        char_code = secret_string[i] + secret_string[i+1] #get the 1st and 2nd numbers of each respective pair from code
-        samp_string += chr(int(char_code) + 22) #convert code back to the string message
+    for i in range(0, len(secret_cstrinng)-1, 2):
+        char_code = secret_cstrinng[i] + secret_cstrinng[i+1] #get the 1st and 2nd numbers of each respective pair from code
+        ceaser_string += chr(int(char_code) + 38) #convert code back to the string message
 
+        new_string = ceaser_string 
+      
+
+print("The new message is: ", new_string)
 
 
