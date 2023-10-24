9efa91adf6e429391a375afef90f859bdb2a7fbf
Mthobeki <mthobekisikhosanag@gmail.com>
2023-10-24 10:48:53 +0200

    update

--- tutorials-from_derek_banas_youtube/alphabets_to_code.py
+++ tutorials-from_derek_banas_youtube/alphabets_to_code.py
@@ -1,17 +1,19 @@
-while True:
-    samp_stirng = input("Enter a message you like to hide: ")
-    secret_string = "" 
-    error_flag = False
+#Enter a string to hide in uppercase: HIDE
+samp_string = input("Enter a string message to hide: ")
 
-    for char in samp_string:
-        if 'A' <= char <= 'z':
-            secret_string += str(ord(char))
-            else:
-                print("Error the message you enetered is not recognised")
-                error_flag = True
-                break 
-    if not:
-        print("Secret message is: ", secret_string)
-        break
+#before we store our secret message string
+secret_string = ""
 
-        
+# cycle through each character in the string
+for char in samp_string:
+    secret_string += str(ord(char) - 22) #stores each character code in new string
+
+print("Secret message is: ", secret_string) #prints the secret message in code
+
+#cycle through each character code 2 numbers at a time since the assigned values come in pairs from 65-90 and we need to seperate them back pairs
+samp_string = ""
+for i in range(0, len(secret_string)-1, 2):
+    char_code = secret_string[i] + secret_string[i+1] #get the 1st and 2nd numbers of each respective pair from code
+    samp_string += chr(int(char_code) + 22) #convert code back to the string message
+
+print("The original message is: ", samp_string)
