4b76b89e9aa738f31e29be1b603111a6e8924d7e
Mthobeki <mthobekisikhosanag@gmail.com>
2023-11-22 11:43:33 +0200

    ceaser cypher

--- tutorials-from_derek_banas_youtube/ceaser_cypher.py
+++ tutorials-from_derek_banas_youtube/ceaser_cypher.py
@@ -34,3 +34,28 @@ print("encrypted messsage: ", secret_message)
 
 # to decrypt 
 key = -key
+
+orig_message = ""
+
+for char in secret_message:
+    if char.isalpha(): 
+        char_code  = ord(char)
+        char_code += key
+        if char.isupper():
+            if char_code > ord('Z'):
+                char_code -= 26
+                # if smaller than A then add 26
+            if char_code < ord('A'):
+                char_code += 26
+
+        else:
+            if char_code > ord('z'):
+                char_code -= 26
+            # if smaller than A then add 26
+            if char_code < ord('a'):
+                char_code += 26
+    # convert code to letter and add to message
+        orig_message += chr(char_code)
+    else:
+        orig_message += char
+print("decrypted messsage: ", orig_message)
